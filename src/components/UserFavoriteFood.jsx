a7a74ad2148ad6af9c5044945d10584e91048fe6
kumar-shivang <shivangkumar1857@gmail.com>
2024-05-26 16:41:43 +0530

    added some inline css

--- src/components/UserFavoriteFood.jsx
+++ src/components/UserFavoriteFood.jsx
@@ -4,7 +4,14 @@ export default function UserFavoriteFood() {
     "section",
     null,
     <h3>Favorite Foods</h3>,
-    <ul>
+    <ul
+      // INFO: We can add css to components like this
+      style={{
+        listStyle: "none",
+        border: "1px solid black",
+        color: "red",
+      }}
+    >
       <li>Food</li>
       <li>Food</li>
       <li>Food</li>
