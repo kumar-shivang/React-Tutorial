19011abfc96451051bffd8f6d4ad551547006604
kumar-shivang <shivangkumar1857@gmail.com>
2024-05-26 23:07:53 +0530

    added login form for event handling

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,10 +1,34 @@
 export default function LoginForm() {
   return (
-    <form>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        let formData = new FormData(e.target);
+        console.log(formData.get("username"));
+        console.log(formData.get("password"));
+        console.log("Form Submitted");
+      }}
+    >
       <label htmlFor="username">Username</label>
-      <input type="text" id="username" />
+      <input
+        type="text"
+        id="username"
+        onChange={(e) => {
+          console.log(e.target.value);
+        }}
+        name="username"
+      />
+      <br />
       <label htmlFor="password">Password</label>
-      <input type="password" id="password" />
+      <input
+        type="password"
+        id="password"
+        onChange={(e) => {
+          console.log(e.target.value);
+        }}
+        name="password"
+      />
+      <br />
       <button type="submit">Login</button>
     </form>
   );
