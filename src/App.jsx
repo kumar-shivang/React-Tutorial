508ab8510a7bcd2c4228addbe83a7fdfdcb6e494
kumar-shivang <shivangkumar1857@gmail.com>
2024-05-26 18:33:23 +0530

    feat: added conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
-import UserProfile from "./components/UserPorfile";
-import UserUsername from "./components/UserUsername";
 export default function App() {
-  const mockUsers = [
-    { id: 1, username: "user1", email: "user1@email.com" },
-    { id: 2, username: "user2", email: "user2@email.com" },
-    { id: 3, username: "user3", email: "user3@email.com" },
-  ];
-  return (
-    <div>
-      {/* <h1>Root Component</h1> */}
-      {/* <UserProfile username="user" email="email@email.com" /> */}
-      {mockUsers.map((user) => {
-        return (
-          <UserProfile
-            key={user.id}
-            username={user.username}
-            email={user.email}
-          />
-        );
-      })}
-      ) )
+  let isBlue = false;
+  return isBlue ? (
+    <div style={{ color: "blue" }}>
+      <h1>Hello World</h1>
+    </div>
+  ) : (
+    <div style={{ color: "red" }}>
+      <h1>Hello World</h1>
     </div>
   );
 }
