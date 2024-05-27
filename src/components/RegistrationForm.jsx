f818eefd326b0b98b16a7169a77e73f9fc63f9de
kumar-shivang <shivangkumar1857@gmail.com>
2024-05-27 14:16:02 +0530

    used state setting

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -1,28 +1,24 @@
 import "./RegistrationForm.css";
 import { useState } from "react";
 export default function RegistrationForm() {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const isDisabled = username === "" || email === "" || password === "";
+  const [formFields, setFormFields] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        let formData = new FormData(e.target);
-        console.log(formData.get("username"));
-        console.log(formData.get("email"));
-        console.log(formData.get("password"));
-        console.log("Form Submitted");
-      }}
-    >
+    <form>
       <label htmlFor="username">Username</label>
       <input
         type="text"
         id="username"
         name="username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
+        value={formFields.username}
+        onChange={(e) =>
+          setFormFields((currentState) => {
+            return { ...currentState, username: e.target.value };
+          })
+        }
       />
       <br />
       <label htmlFor="email">Email</label>
@@ -30,8 +26,12 @@ export default function RegistrationForm() {
         type="email"
         id="email"
         name="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        value={formFields.email}
+        onChange={(e) =>
+          setFormFields((currentState) => {
+            return { ...currentState, email: e.target.value };
+          })
+        }
       />
       <br />
       <label htmlFor="password">Password</label>
@@ -39,13 +39,15 @@ export default function RegistrationForm() {
         type="password"
         id="password"
         name="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        value={formFields.password}
+        onChange={(e) =>
+          setFormFields((currentState) => {
+            return { ...currentState, password: e.target.value };
+          })
+        }
       />
       <br />
-      <button type="submit" disabled={isDisabled}>
-        Register
-      </button>
+      <button type="submit">Register</button>
     </form>
   );
 }
