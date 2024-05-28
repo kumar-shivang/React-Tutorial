11ae51072c8f7d43b230702c952840fbb7d70b05
kumar-shivang <shivangkumar1857@gmail.com>
2024-05-28 15:31:32 +0530

    added abort control

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { func } from "prop-types";
 import { useState, useEffect } from "react";
 export default function App() {
   const [counter, setCounter] = useState(0);
@@ -6,6 +7,40 @@ export default function App() {
     console.log("useEffect called");
     document.title = `Counter: ${counter}`;
   }, [sync]);
+  useEffect(() => {
+    // fetch("https://jsonplaceholder.typicode.com/users", {
+    //   method: "GET",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    // })
+    //   .then((response) => response.json())
+    //   .then((data) => console.log(data))
+    //   .catch((error) => console.log(error));
+    const controller = new AbortController();
+    async function fetchUsers() {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            signal: controller.signal,
+          },
+        );
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchUsers();
+    return () => {
+      controller.abort();
+    };
+  });
   return (
     <>
       <h1>Counter: {counter}</h1>
