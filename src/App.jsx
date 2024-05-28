1ce497fc6dc11d25490ff2ef40730eb270811d6d
kumar-shivang <shivangkumar1857@gmail.com>
2024-05-28 19:21:36 +0530

    some changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,21 @@
-import { func } from "prop-types";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import useDocumentClick from "./utils/hooks/useDocumentClick";
+import ToggleElement from "./components/ToggleElement";
 export default function App() {
-  const [counter, setCounter] = useState(0);
-  const [sync, setSync] = useState(false);
-  useEffect(() => {
-    console.log("useEffect called");
-    document.title = `Counter: ${counter}`;
-  }, [sync]);
-  useEffect(() => {
-    // fetch("https://jsonplaceholder.typicode.com/users", {
-    //   method: "GET",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    // })
-    //   .then((response) => response.json())
-    //   .then((data) => console.log(data))
-    //   .catch((error) => console.log(error));
-    const controller = new AbortController();
-    async function fetchUsers() {
-      try {
-        const response = await fetch(
-          "https://jsonplaceholder.typicode.com/users",
-          {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            signal: controller.signal,
-          },
-        );
-        const data = await response.json();
-        console.log(data);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    fetchUsers();
-    return () => {
-      controller.abort();
-    };
+  const [toggle, setToggle] = useState(true);
+  useDocumentClick(() => {
+    console.log("Document clicked");
   });
   return (
-    <>
-      <h1>Counter: {counter}</h1>
-      <button onClick={() => setCounter(counter + 1)}>Increment</button>
-      <button onClick={() => setSync((current) => !current)}>Sync</button>
-    </>
+    <div>
+      {toggle && <ToggleElement />}
+      <button
+        onClick={() => {
+          setToggle(!toggle);
+        }}
+      >
+        Toggle
+      </button>
+    </div>
   );
 }
