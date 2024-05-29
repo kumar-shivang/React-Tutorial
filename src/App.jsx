78b09e58de1c5beec17e046c122c8a606cea5bb5
kumar-shivang <shivangkumar1857@gmail.com>
2024-05-29 17:50:34 +0530

    learnt about router and router location state

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
 import { useState } from "react";
 import UserProfile from "./components/UserProfile";
+import { Outlet, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 export default function App() {
   const [userId, setUserId] = useState(1);
+  const navigate = useNavigate();
   return (
     <div>
+      <nav>
+        <ul>
+          <Link to="/">Home</Link>
+          <Link to="/users">Users</Link>
+          <Link to="/blogs">Blogs</Link>
+        </ul>
+      </nav>
       <input
-        type="number"
-        max={10}
-        min={1}
-        onChange={(e) => setUserId(e.target.value)}
+        onChange={(e) => {
+          if (e.target.value.length == 10) {
+            navigate("/blogs", {
+              state: {
+                blogs: [
+                  {
+                    id: 1,
+                    title: "hello world",
+                    content:
+                      "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+                  },
+                ],
+              },
+            });
+          }
+        }}
       />
       <UserProfile userId={userId} />
+      <Outlet />
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import Blogs from "./components/Blogs";
+const router = new createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/users",
+        element: <h1>Users</h1>,
+      },
+      {
+        path: "/blogs",
+        element: <Blogs />,
+      },
+    ],
+  },
+  {
+    path: "/user",
+    element: <h1>Heading</h1>,
+  },
+]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
